f33c808f2c2f145aa4efd75a8fefebedf892e2f2
AmanSalman <rapamansalman@gmail.com>
2024-03-31 06:11:55 +0300

    now

--- src/modules/app.router.js
+++ src/modules/app.router.js
@@ -3,17 +3,23 @@ import userRouter from './user/user.router.js';
 import bookRouter from './book/book.router.js';
 import categoryRouter from './category/category.router.js';
 import orderRouter from './order/order.router.js';
-export const initApp = (app,express)=>{
+import cors from 'cors'; // Import cors middleware
+
+export const initApp = (app, express) => {
     app.use(express.json());
-    app.get('/',(req,res)=>{
-        return res.json({message:'welcome'})
-    })
+    app.use(cors()); // Enable CORS middleware globally
+
+    app.get('/', (req, res) => {
+        return res.json({ message: 'welcome' });
+    });
+
     app.use('/auth', authRouter);
     app.use('/user', userRouter);
     app.use('/book', bookRouter);
-    app.use('/category',categoryRouter); 
-    app.use('/order',orderRouter);
-    app.use('*', (req,res)=>{
-        return res.json ({message:'page not found'})
-    })
-}
+    app.use('/category', categoryRouter); 
+    app.use('/order', orderRouter);
+
+    app.use('*', (req, res) => {
+        return res.json({ message: 'page not found' });
+    });
+};
