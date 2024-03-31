fbcd7a0cb1f63b1ea921628023b8717ab817fac1
AmanSalman <rapamansalman@gmail.com>
2024-03-31 04:52:49 +0300

    changes

--- index.js
+++ index.js
@@ -14,7 +14,13 @@ const app = express();
 //   };  
   
   
-  app.use(cors());
+//   app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
   
 dotenv.config();
 const PORT = process.env.PORT;
