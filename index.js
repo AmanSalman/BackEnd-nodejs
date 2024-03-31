607139d758790134214d53b019770ac0102c141d
AmanSalman <rapamansalman@gmail.com>
2024-03-31 06:02:47 +0300

    please

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 
 const corsOptions = {
-    origin: '*',
+    origin: 'http://localhost:5173/',
     methods:['GET,HEAD,PUT,PATCH,POST,DELETE'],
     credentials: true,
     optionsSuccessStatus: 200,
