e0ad9eea334d9d96f19a776da703fd1cc080416d
AmanSalman <rapamansalman@gmail.com>
2024-04-01 04:49:26 +0300

    changes

--- src/modules/order/order.router.js
+++ src/modules/order/order.router.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import * as orderController from './order.controller.js'
+import auth from "../../middleware/auth.js";
 const router = Router({caseSensitive:true});
 
 // const isAdmin = (req, res, next) => {
@@ -19,6 +20,6 @@ router.post('/create', orderController.createOrder); //user
 router.delete('/delete/:_id', orderController.destroy); //user
 router.get('/getAllOrders', orderController.GetOrders); //admin
 router.put('/AcceptAllOrders', orderController.AcceptAllOrders); //admin
-router.put('/AcceptOrder/:_id', orderController.AcceptOrder) //admin
-router.put('/rejectOrder/:_id', orderController.RejectOrder);
+router.put('/AcceptOrder/:_id', auth, orderController.AcceptOrder) //admin
+router.put('/rejectOrder/:_id',auth, orderController.RejectOrder);
 export default router;
