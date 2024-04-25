f9a6771d14ec1be3b14064fe300d52640fd97b5f
AmanSalman <rapamansalman@gmail.com>
2024-04-26 00:17:13 +0300

    changes

--- index.js
+++ index.js
@@ -32,5 +32,5 @@ connectDB().then(()=>{
     
     app.listen(PORT, ()=>{
         console.log(`Server is running on port ${PORT}`)
-    })
+    }) 
 })
--- src/modules/user/user.router.js
+++ src/modules/user/user.router.js
@@ -3,12 +3,12 @@ import * as UserController from './user.controller.js';
 import { Router } from 'express';
 import asyncHandler from '../../services/errorHandling.js';
 const router = Router ();
-router.get('/users',auth, ensureUserAdmin('admin'),UserController.Users);
+router.get('/users',auth ,UserController.Users);
 router.put('/update/', auth, UserController.update);
 router.delete('/delete/:_id', UserController.destroy);
 router.get('/getOrders/:userId',asyncHandler(UserController.getOrders) );
 router.get('/profile', auth, asyncHandler(UserController.Profile) );
-router.patch('/disable/:_id', auth,ensureUserAdmin('admin'), asyncHandler(UserController.Disable)); 
-router.patch('/activate/:_id', auth, ensureUserAdmin('admin'), UserController.Activate);
+router.patch('/disable/:_id', auth, asyncHandler(UserController.Disable)); 
+router.patch('/activate/:_id', auth, UserController.Activate);
 export default router;
  
